9dc2e598024ad5736df2ade055122572e3212bb3
Crisi Sanchez Pineda <harmaharcri@hotmail.com>
2020-08-29 13:55:38 -0500

    Added the persistence project and the database util classes

---
+++ Tasker/src/co/uniquindio/semester7/model/exception/EntityNullException.java
@@ -0,0 +1,14 @@
+package co.uniquindio.semester7.model.exception;
+
+public class EntityNullException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public EntityNullException(String errorMessage) {
+		super(errorMessage);
+	}
+
+}
---
+++ Tasker/src/co/uniquindio/semester7/model/exception/EntityRepeatexception.java
@@ -0,0 +1,14 @@
+package co.uniquindio.semester7.model.exception;
+
+public class EntityRepeatexception extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public EntityRepeatexception(String errorMessage) {
+		super(errorMessage);
+	}
+
+}
